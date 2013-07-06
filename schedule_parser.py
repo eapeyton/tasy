504903086b7e24ba69e2060a7a3f662fb9886494
Eric Peyton <ea.peyton@gmail.com>
2013-07-06 01:09:27 -0400

    Adding get_match_index method.

--- schedule_parser.py
+++ schedule_parser.py
@@ -8,11 +8,18 @@ class MyHTMLParser(HTMLParser):
         self.match_pat = re.compile("matchup[0-9]*")
         self.match_index = 0
         
+
+    def get_match_index(self, attribute):
+        result = re.search(r'\d+',attribute[1])
+        return int(result.group())
+    
     def handle_starttag(self, tag, attrs):
         for attribute in attrs:
-            if is_matchup(attribute):
-                print(att + ":" + value)
+            if self.is_matchup(attribute):
+                self.active = True
+                self.match_index = self.get_match_index(attribute)
         if self.active:
+            print(self.match_index)
             for att,value in attrs:
                 if att == "href" and "teamid" in value.lower():
                     print(att + ":" + value)
@@ -22,11 +29,12 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         pass
         
-    def is_matchup(attribute):
+    def is_matchup(self,attribute):
         name = attribute[0]
         value = attribute[1]
         if name == "name" and self.match_pat.match(value):
-            print(att + ":" + value)
+            return True
+        return False
         
 
 parser = MyHTMLParser()
